6719887661a9f9bb428f743ce0d9f491d3e2cf3b
Stefan Chu <stefanchu@live.com>
2022-04-06 10:20:37 -0700

    Added addCookie Form

---
+++ addCookie.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class addCookie
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cookieNameTB = New System.Windows.Forms.TextBox()
+        Me.submit = New System.Windows.Forms.Button()
+        Me.cancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(143, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(188, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Add Cookie"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name of Cookie"
+        '
+        'cookieNameTB
+        '
+        Me.cookieNameTB.Location = New System.Drawing.Point(151, 94)
+        Me.cookieNameTB.Name = "cookieNameTB"
+        Me.cookieNameTB.Size = New System.Drawing.Size(188, 29)
+        Me.cookieNameTB.TabIndex = 2
+        '
+        'submit
+        '
+        Me.submit.Location = New System.Drawing.Point(188, 315)
+        Me.submit.Name = "submit"
+        Me.submit.Size = New System.Drawing.Size(86, 34)
+        Me.submit.TabIndex = 3
+        Me.submit.Text = "Submit"
+        Me.submit.UseVisualStyleBackColor = True
+        '
+        'cancel
+        '
+        Me.cancel.Location = New System.Drawing.Point(16, 315)
+        Me.cancel.Name = "cancel"
+        Me.cancel.Size = New System.Drawing.Size(75, 34)
+        Me.cancel.TabIndex = 4
+        Me.cancel.Text = "Cancel"
+        Me.cancel.UseVisualStyleBackColor = True
+        '
+        'addCookie
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.cancel)
+        Me.Controls.Add(Me.submit)
+        Me.Controls.Add(Me.cookieNameTB)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "addCookie"
+        Me.Text = "Add Cookie"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cookieNameTB As TextBox
+    Friend WithEvents submit As Button
+    Friend WithEvents cancel As Button
+End Class
---
+++ addCookie.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class addCookie
+    Private Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        Dim cookieName As String = cookieNameTB.Text
+        Dim conn As New myConnection()
+        Dim command As New MySqlCommand("INSERT INTO `cookie`(`name`) VALUES (@cookieName)", conn.getConnection())
+        command.Parameters.Add("@cookieName", MySqlDbType.VarChar).Value = cookieName
+        conn.openConnection()
+
+        If command.ExecuteNonQuery() = 1 Then
+            MsgBox("COOKIE INSERTED")
+            conn.closeConnection()
+        Else
+            MsgBox("COOKIE NOT INSERTED")
+            conn.closeConnection()
+        End If
+    End Sub
+End Class
