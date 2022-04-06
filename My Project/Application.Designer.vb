d28c414c25bb45e93a5c81326bb729aafe3e0d8e
Stefan Chu <stefanchu@live.com>
2022-04-05 19:24:23 -0700

    Change startup target framework to main menu

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Troop1521CookieInventory2.userForm
+            Me.MainForm = Global.Troop1521CookieInventory2.mainMenu
         End Sub
     End Class
 End Namespace
